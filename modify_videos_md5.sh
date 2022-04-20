377715db2cf4eaffb83d713b2b42a4caa5518a6c
tisfeng <tisfeng@gmail.com>
2022-04-20 22:59:38 +0800

    add restore_videos_md5.sh

--- modify_videos_md5.sh
+++ modify_videos_md5.sh
@@ -5,12 +5,6 @@
 # 用途：批量修改该脚本所在目录下所有文件的md5
 #===============================================================================#
 
-
-# echo `ls $0`
-echo `ls $1`
-
-exit 0
-
 # 获取该脚本所在的目录路径
 script_dir=$(dirname $0)
 echo $script_dir
@@ -18,6 +12,29 @@ echo $script_dir
 # 防止文件名中有空格
 OLD_IFS=$IFS
 IFS=$'\n'
+
+
+# 函数：替换文件名中的空格
+function replace_space()
+{
+    for file in `ls $script_dir`
+    do
+        echo $file
+        if [ -d $1"/"$file ] 
+        then
+            replace_space $1"/"$file
+        else
+            # 替换文件名中的空格
+            new_file=`echo $file | sed 's/ /_/g'`
+            if [ $file != $new_file ]
+            then
+                mv $1"/"$file $1"/"$new_file
+            fi
+        fi
+    done
+}
+
+
 
 # 遍历所有文件名
 for file_name in $(ls $script_dir); do
@@ -50,12 +67,21 @@ for file_name in $(ls $script_dir); do
         # 生成一个0～9的随机数
         random_num=$((RANDOM % 10))
         # 待拼接的字符串为#+随机数
-        str="#$random_num"
-        # 打印字符串
-        echo $str
+        append_str="#$random_num"
+        # 打印拼接字符串
+        echo $append_str
+        
+        # 获取文件最后一行字符串
+        last_line=$(tail -n 1 $new_file_path)
+        # 如果字符串的前缀是'#'
+        if [ ${last_line:0:1} == "#" ]; then
+           # 将拼接字符串不换行追加到文件末尾
+              echo -e "$last_line$append_str" >> $new_file_path
+           
+            # 打印字符串
+            echo $last_line
+        fi
 
-        #将字符串追加到文件末尾
-        echo $str >> $new_file_path
 
         # 打印新文件的md5
         md5sum $new_file_name
