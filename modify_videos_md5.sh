c792a12d1be61e42bd557f7b23797dc74871bc7b
tisfeng <tisfeng@gmail.com>
2022-04-23 01:25:52 +0800

    更新说明：修改脚本所在目录->修改当前工作目录

--- modify_videos_md5.sh
+++ modify_videos_md5.sh
@@ -2,17 +2,14 @@
 # script.sh
 
 #===============================================================================#
-# 用途：批量修改该脚本所在目录下所有文件的md5
+# 用途：批量修改当前目录下所有文件的md5
 #===============================================================================#
-
-# 获取该脚本所在的目录路径
-script_dir=$(dirname $0)
 
 # 防止文件名中有空格
 OLD_IFS=$IFS
 IFS=$'\n'
 
-# 函数：将当前目录下所有文件名中的空格用下划线替换
+# 函数：将当前工作目录下所有文件名中的空格用下划线替换
 function replace_space_to_underline() {
     for file in $(ls); do
         echo $file
@@ -22,7 +19,7 @@ function replace_space_to_underline() {
     done
 }
 
-# 函数：递归遍历当前目录下所有文件，在文件末尾追加字符串"#1024"。
+# 函数：递归遍历当前工作目录下所有文件，在文件末尾追加字符串"#1024"。
 function append_string_to_file_recursive() {
     for file in $(ls); do
         if [ -f $file ]; then
@@ -40,6 +37,7 @@ function append_string_to_file_recursive() {
     done
 }
 
+echo '当前工作目录为:' $PWD
 echo '//====================开始批量修改文件md5==================//'
 append_string_to_file_recursive
 echo '//====================修改成功==================//'
